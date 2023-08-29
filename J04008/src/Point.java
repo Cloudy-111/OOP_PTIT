f3971d2f1b2e7805df09a82c34e63baa7373ee21
Diep <diepani303@gmail.com>
2023-08-29 10:20:35 +0700

    Chu vi tam giác

---
+++ J04008/src/Point.java
@@ -0,0 +1,35 @@
+public class Point {
+    double x, y, dist, perimeter;
+    public Point(){}
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public Point(Point p){
+        this.x = p.x;
+        this.y = p.y;
+    }
+    public double getX(){
+        return this.x;
+    }
+    public double getY(){
+        return this.y;
+    }
+    public void setDist(double dist){
+        this.dist = dist;
+    }
+    public double distance(Point secondPoint){
+        return Math.sqrt(Math.pow(this.x - secondPoint.x, 2) + Math.pow(this.y - secondPoint.y, 2));
+    }
+    public double distance(Point a, Point b){
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+    }
+    public void setPerimeter(double perimeter){
+        this.perimeter = perimeter;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%.3f", perimeter);
+    }
+}
