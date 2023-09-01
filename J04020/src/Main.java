c6f27cfa136ec1f7053340b50d7f6475717bae3b
Diep <diepani303@gmail.com>
2023-09-01 10:32:18 +0700

    Lớp Pair

---
+++ J04020/src/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            boolean check = false;
+            for(int i = 2; i <= 2*Math.sqrt(n); i++){
+                Pair<Integer, Integer> p = new Pair<>(i, n-i);
+                if(p.isPrime()){
+                    System.out.println(p);
+                    check = true;
+                    break; 
+                }
+            }
+            if(!check) System.out.println(-1);
+        }
+    }
+    
+}
