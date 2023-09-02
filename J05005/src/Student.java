0f732ba3802bdd7dbf261f9709e5b80cf6532641
Diep <diepani303@gmail.com>
2023-09-02 15:06:33 +0700

    Danh sách đối tượng sinh viên - 3

---
+++ J05005/src/Student.java
@@ -0,0 +1,56 @@
+public class Student implements Comparable<Student>{
+    private int id;
+    private String name, birthday, grade;
+    private double GPA;
+    
+    public Student(){
+        this.name = "";
+        this.birthday = "";
+        this.grade = "";
+        this.id = 0;
+        this.GPA = 0;
+    }
+    public double getGPA(){
+        return this.GPA;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name){
+        name = name.toLowerCase();
+        String[] tmp = name.split(" ");
+        String res = "";
+        for(int i = 0; i < tmp.length; ++i){
+            if(!tmp[i].isEmpty())res += tmp[i].substring(0,1).toUpperCase() + tmp[i].substring(1) + " ";
+        }
+        this.name = res;
+    }
+    public void setGrade(String grade){
+        this.grade = grade;
+    }
+    public void setGPA(double GPA){
+        this.GPA = GPA;
+    }
+    public void setBirthday(String birthday){
+        String[] tmp = birthday.split("/");
+        String res = "";
+        for(int i = 0; i < tmp.length; ++i){
+            if(tmp[i].length() == 1 )res += "0" + tmp[i];
+            else res += tmp[i];
+            if(i != tmp.length - 1)res += "/";
+        }
+        this.birthday = res;
+    }
+    
+    @Override
+    public int compareTo(Student a){
+        if(GPA > a.getGPA())return -1;
+        else if(GPA < a.getGPA())return 1;
+        else return 0;
+    }
+    @Override
+    public String toString(){
+        return String.format("B20DCCN%03d %s%s %s %.2f", id, name, grade, birthday, GPA);
+    }
+}
+
