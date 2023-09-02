a3c34f499099ca04684aa22028b4bb1f2dce0833
Diep <diepani303@gmail.com>
2023-09-02 09:02:47 +0700

    Lớp INTSET

---
+++ J04021/src/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), a[] = new int[n], b[] = new int[m];
+        for(int i = 0; i<n; i++) a[i] = sc.nextInt();
+        for(int i = 0; i<m; i++) b[i] = sc.nextInt();
+        IntSet s1 = new IntSet(a);
+        IntSet s2 = new IntSet(b);
+        IntSet s3 = s1.union(s2);
+        System.out.println(s3);
+
+    }
+}
