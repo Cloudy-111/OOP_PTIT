016041652912d1dbf072decf55ad4082bc1d0beb
Diep <diepani303@gmail.com>
2023-09-07 12:58:43 +0700

    Liệt kê từ khác nhau

---
+++ J07007/src/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        WordSet ws = new WordSet("VANBAN.in");
+        System.out.println(ws);
+    }
+    
+}
