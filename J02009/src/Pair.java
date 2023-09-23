5570b51810ac3d04f10f61f0e5c8d8e89977e3c6
Diep <diepani303@gmail.com>
2023-09-23 10:54:41 +0700

    Xếp hàng

---
+++ J02009/src/Pair.java
@@ -0,0 +1,14 @@
+public class Pair<F, S>{
+    private F first;
+    private S second;
+    public Pair(F first, S second){
+        this.first = first;
+        this.second = second;
+    }
+    public F getFirst(){
+        return first;
+    }
+    public S getSecond(){
+        return second;
+    }
+}
