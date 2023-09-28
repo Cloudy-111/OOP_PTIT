fcde94954116419585f011513677b6be05dc5415
Diep <diepani303@gmail.com>
2023-09-28 10:37:08 +0700

    Danh sách Doanh nghiệp nhận dinh viên thực tập - 2

---
+++ J05029/src/Company.java
@@ -0,0 +1,21 @@
+public class Company implements Comparable<Company>{
+    private String id, name;
+    private int num;
+    public Company(String id, String name, int num){
+        this.id = id;
+        this.name = name;
+        this.num = num;
+    }
+    public int getNum(){
+        return this.num;
+    }
+    @Override
+    public int compareTo(Company b){
+        if(b.num == this.num)return this.id.compareTo(b.id);
+        return b.num - this.num;
+    }
+    @Override
+    public String toString(){
+        return id + " " + name + " " + num;
+    }
+}
