087df63befbad9df5f866e1b2b62d97b880a74e9
Diep <diepani303@gmail.com>
2023-10-05 10:32:24 +0700

    Quản lý Bài tập nhóm - 1

---
+++ J06003/src/BTL.java
@@ -0,0 +1,12 @@
+public class BTL {
+    private String name;
+    private int ord;
+    public BTL(String name, int ord){
+        this.name = name;
+        this.ord = ord;
+    }
+    @Override
+    public String toString(){
+        return name;
+    }
+}
