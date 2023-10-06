0a622a88000e4d673ac80b1b0de44632cc8b540f
Diep <diepani303@gmail.com>
2023-10-06 11:07:37 +0700

    Hợp và giao của 2 file văn bản

---
+++ J07014/src/Main.java
@@ -0,0 +1,18 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        WordSet s1 = new WordSet("DATA1.in");
+        WordSet s2 = new WordSet("DATA2.in");
+        System.out.println(s1.union(s2));
+        System.out.println(s1.intersection(s2));
+    }
+    
+}
