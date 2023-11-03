8bfe4be111c50647079ef3d0297341b375d489f7
Diep <diepani303@gmail.com>
2023-11-03 21:53:18 +0700

    Liệt kê cặp số - 2

---
+++ J07042/src/Pair.java
@@ -0,0 +1,28 @@
+package test;
+//phải thêm package test vào để AC, còn không thì toàn bị IR
+import java.io.*;
+
+public class Pair implements Serializable, Comparable<Pair>{
+    private int first, second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+    
+    @Override
+    public int compareTo(Pair o) {
+        return this.first - o.first;
+    }
+    public String toString(){
+        return "(" + first + ", " + second + ")";
+    }
+}
