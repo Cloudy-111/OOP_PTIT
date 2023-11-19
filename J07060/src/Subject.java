75bc082efda1bd6197984ef16a9b5db4f84ecde0
Diep <diepani303@gmail.com>
2023-11-19 15:58:51 +0700

    Sắp xếp lịch thi

---
+++ J07060/src/Subject.java
@@ -0,0 +1,22 @@
+public class Subject {
+    private String idSub, nameSub, form;
+    
+    public Subject(String idSub, String nameSub, String form){
+        this.idSub = idSub;
+        this.nameSub = nameSub;
+        this.form = form;
+    }
+
+    public String getIdSub() {
+        return idSub;
+    }
+
+    public String getNameSub() {
+        return nameSub;
+    }
+
+    public String getForm() {
+        return form;
+    }
+    
+}
