d169ae5769b210c59b9930a76c9261819b0b0f4a
Diep <diepani303@gmail.com>
2023-12-18 15:49:11 +0700

    Biến đổi S - T

---
+++ J08026/src/Pair.java
@@ -0,0 +1,16 @@
+public class Pair<F, S> {
+    private F first;
+    private S second;
+    public Pair(F first, S second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+}
